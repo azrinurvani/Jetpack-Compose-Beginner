466cc4edd59c055e79a39e9474058d1cb824dd2d
azrinurvani <azrinurvani@gmail.com>
2024-07-11 15:44:49 +0700

    Manifest Convertor Unit Test (TODO 84)

---
+++ app/src/test/java/com/azrinurvani/jetpackcomposeforbeginner/RoverManifestConvertorTest.kt
@@ -0,0 +1,63 @@
+package com.azrinurvani.jetpackcomposeforbeginner
+
+import com.azrinurvani.jetpackcomposeforbeginner.domain.model.RoverManifestUiModel
+import com.azrinurvani.jetpackcomposeforbeginner.domain.model.RoverManifestUiState
+import com.azrinurvani.jetpackcomposeforbeginner.domain.model.toUiModel
+import com.azrinurvani.jetpackcomposeforbeginner.service.model.ManifestPhotoRemoteModel
+import com.azrinurvani.jetpackcomposeforbeginner.service.model.PhotoManifestRemoteModel
+import com.azrinurvani.jetpackcomposeforbeginner.service.model.RoverManifestRemoteModel
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+//TODO 84 - Create Unit Test for Rover Manifest Convertor (fro RoverManifestRemoteModel to RoverManifestUiState)
+class RoverManifestConvertorTest {
+
+    @Test
+    fun `should convert roverManifestRemoteModel to RoverManifestUiState`(){
+        //Given
+        val roverManifestRemoteModel = RoverManifestRemoteModel(
+            photoManifest = PhotoManifestRemoteModel(
+                landingDate = "2021-02-18",
+                launchDate = "2020-07-30",
+                maxDate = "2023-05-19",
+                maxSol = "799",
+                name = "Perseverance",
+                photos = listOf(
+                    ManifestPhotoRemoteModel(
+                        cameras = listOf("REAR_HAZCAM_LEFT","REAR_HAZCAM_RIGHT"),
+                        earthDate = "2021-02-18",
+                        sol = 0,
+                        totalPhotos = 54
+                    ),
+                    ManifestPhotoRemoteModel(
+                        cameras = listOf("EDL_DDCAM","EDL_PUCAM2"),
+                        earthDate = "2021-02-19",
+                        sol = 1,
+                        totalPhotos = 201
+                    )
+                ),
+                status = "active",
+                totalPhotos = 156877
+            )
+        )
+
+        //When
+        val result = toUiModel(roverManifestRemoteModel)
+
+        //Then
+        val expectedResult = RoverManifestUiState.Success(listOf(
+            RoverManifestUiModel(
+                sol = "1",
+                earthDate = "2021-02-19",
+                photoNumber = "201"
+            ),
+            RoverManifestUiModel(
+                sol = "0",
+                earthDate = "2021-02-18",
+                photoNumber = "54"
+            )
+        ))
+
+        assertEquals(expectedResult,result)
+    }
+}
